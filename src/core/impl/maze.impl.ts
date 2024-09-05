4e35356cc603ea0b7dcf769f20b90c457a9bed65
ferchaparro <chaparro3214@gmail.com>
2024-09-04 18:44:11 -0700

    Update maze.impl.ts

--- src/core/impl/maze.impl.ts
+++ src/core/impl/maze.impl.ts
@@ -135,7 +135,7 @@ export class MazeImpl implements Maze, MazeInfo {
             return -1;
         }
         this._maze = new MazeConfig(this._file);
-        await this._maze.load();
+        await Sleeper.sleep(200)
         this._screen = new MazeScreen(this, this.canvas, this.ctx);
         this._x = this._maze!.iX();
         this._y = this._maze!.iY();
