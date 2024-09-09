a987ea390481558f12563fcec851b3fbd35cf61a
Ferc <chaparro3214@gmail.com>
2024-09-08 20:37:01 -0700

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        ranking: 'src/ranking.html',
+        ranking: 'ranking.html',
       }
     }
   }
